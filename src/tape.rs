b4365c0ce6bdd07d3061408bc716448fbcaf9fb9
hadrian-reppas <hadrian@uchicago.edu>
2023-04-03 12:59:55 -0500

    print final tape

--- src/tape.rs
+++ src/tape.rs
@@ -1,11 +1,63 @@
-use bigdecimal::{BigDecimal, ToPrimitive};
+use std::iter::Peekable;
+
+use bigdecimal::BigDecimal;
 use num_bigint::BigInt;
+use unicode_segmentation::UnicodeSegmentation;
 
 pub fn dump(tape: &[&str], terminal_width: usize) {
-    println!("terminal size: {:?}", termion::terminal_size());
-    println!("tape: {tape:?}\n");
+    if tape.is_empty() {
+        println!("┌──┬──┬");
+        println!("│  │  │");
+        println!("└──┴──┴");
+    }
+
+    let mut symbols = tape.iter().copied().peekable();
+    while symbols.peek().is_some() {
+        let line = next_line(&mut symbols, terminal_width);
+        print_line(&line);
+    }
+    println!();
 }
 
+fn next_line<'a>(
+    symbols: &mut Peekable<impl Iterator<Item = &'a str>>,
+    width: usize,
+) -> Vec<&'a str> {
+    let mut len = 1;
+    let mut line = Vec::new();
+    while line.is_empty()
+        || symbols
+            .peek()
+            .map_or(false, |s| len + s.graphemes(true).count() + 3 <= width)
+    {
+        let symbol = symbols.next().unwrap();
+        line.push(symbol);
+        len += symbol.graphemes(true).count() + 3;
+    }
+    line
+}
+
+fn print_line(symbols: &[&str]) {
+    print!("┬");
+    for symbol in symbols {
+        print!("{}┬", "─".repeat(2 + symbol.graphemes(true).count()));
+    }
+    println!();
+
+    print!("│");
+    for symbol in symbols {
+        print!(" {symbol} │");
+    }
+    println!();
+
+    print!("┴");
+    for symbol in symbols {
+        print!("{}┴", "─".repeat(2 + symbol.graphemes(true).count()));
+    }
+    println!();
+}
+
+// TODO: fix this
 pub fn print_decimal(tape: &[&str], radix: u32, start: usize, stride: usize) {
     let tape: String = tape
         .iter()
@@ -23,7 +75,11 @@ pub fn print_decimal(tape: &[&str], radix: u32, start: usize, stride: usize) {
         let power = BigInt::from(radix).pow(tape.len() as u32);
         let coeff = BigDecimal::new(power, 0).with_prec(1000);
         let result = format!("{}", decimal / coeff);
-        if result.chars().filter(|c| c.is_ascii_digit()).all(|c| c == '0') {
+        if result
+            .chars()
+            .filter(|c| c.is_ascii_digit())
+            .all(|c| c == '0')
+        {
             print!("0.0");
         } else {
             print!("{result}");
